2666bf90fcd9bd8817d6052a2349eaedd0b4ac64
Guilherme Rocha <85852982+GuikRocha@users.noreply.github.com>
2024-06-27 16:12:35 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,25 +3,30 @@
 // ... (código das funções globalInsert, instanceStyle e init) ...
 
 function start(options) {
- globalInsert();
- if (gInsertedScript) {
-  gInsertedScript.addEventListener("load", function () {
-   init(options);
-  });
- } else {
-  init(options);
- }
+  globalInsert();
+
+  if (gInsertedScript) {
+    gInsertedScript.addEventListener("load", function () {
+      init(options);
+    });
+  } else {
+    init(options);
+  }
 }
 
 // Configuração do player (o embed é obtido da URL)
 const playerOptions = {
- id: 'player',
- loop: true,
- color: 'red',
- radius: '10',
- controls: ['play-large', 'play', 'progress', 'current-time', 'mute', 'volume', 'captions', 'settings', 'pip', 'airplay', 'fullscreen'],
- settings: ['captions', 'quality', 'speed', 'loop'],
- autoplay: false
+  id: 'player',
+  loop: true,
+  color: 'red',
+  radius: '10',
+  controls: ['play-large', 'play', 'progress', 'current-time', 'mute', 'volume', 'captions', 'settings', 'pip', 'airplay', 'fullscreen'],
+  settings: ['captions', 'quality', 'speed', 'loop'],
+  autoplay: false, // Desabilita o autoplay para dispositivos móveis
+  i18n: {
+    speed: 'Velocidade',
+    quality: 'Qualidade'
+  }
 };
 
 // Obtendo o ID do vídeo da URL
