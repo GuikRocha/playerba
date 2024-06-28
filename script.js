7a84242c6163b4314cff4c4415636c5b82c4db61
Guilherme Rocha <85852982+GuikRocha@users.noreply.github.com>
2024-06-28 14:44:15 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,45 @@
-const player = new Plyr('#player', {
-  youtube: {
-    noCookie: true,   // Evita cookies do YouTube
-    modestbranding: 1, // Oculta o logo do YouTube
-    showinfo: 0,      // Oculta informações do vídeo
-    rel: 0,           // Não mostra vídeos relacionados
-    iv_load_policy: 3 // Oculta anotações em vídeo
-  },
-  controls: [
-    'play-large', 
-    'play', 
-    'progress', 
-    'current-time', 
-    'mute', 
-    'volume',
-    'fullscreen'
-  ],
-  i18n: {
-    speed: 'Velocidade',
-    quality: 'Qualidade'
+"use strict";
+
+let gInserted = false;
+let gInsertedScript = false;
+let unmute = false;
+
+function globalInsert() {
+  // ... (código da função globalInsert) ...
+}
+
+function instanceStyle(id, color, radius) {
+  // ... (código da função instanceStyle) ...
+}
+
+function init(options) {
+  // ... (código da função init) ...
+}
+
+function start(options) {
+  globalInsert();
+  if (gInsertedScript) {
+    gInsertedScript.addEventListener("load", function () {
+      init(options);
+    });
+  } else {
+    init(options);
   }
-});
+}
+
+// Configuração do player (o embed é obtido da URL)
+const playerOptions = {
+  id: 'player',
+  loop: false,
+  color: 'red',
+  radius: '10',
+  controls: ['play-large', 'play', 'progress', 'current-time', 'mute', 'volume', 'captions', 'settings', 'pip', 'airplay', 'fullscreen'],
+  settings: ['captions', 'quality', 'speed', 'loop'],
+  autoplay: false,
+};
+
+// Obtendo o ID do vídeo da URL
+const urlParams = new URLSearchParams(window.location.search);
+playerOptions.embed = urlParams.get('embed'); 
+
+start(playerOptions);
